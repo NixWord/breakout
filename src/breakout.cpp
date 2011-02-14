ad86b4d26e1b886ab87fc06725121d0967d9b48e
NixWord <lewinp@esiee.fr>
2011-02-14 23:03:16 +0100

    Creating main SDL main loop

--- src/breakout.cpp
+++ src/breakout.cpp
@@ -1,18 +1,40 @@
-/*
- ============================================================================
- Name        : breakout.cpp
- Author      : 
- Version     :
- Copyright   : Copyleft
- Description : Hello World in C++,
- ============================================================================
- */
-
-#include <iostream>
-
-using namespace std;
+#include <SDL/SDL.h>
 
 int main(void) {
-	cout << "Hello World" << endl; /* prints Hello World */
-	return 0;
+	SDL_Surface* screen = NULL;
+	SDL_Event event;
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+	SDL_WM_SetCaption("Breakout", NULL);
+
+	bool loop = true;
+	while (loop) {
+		SDL_WaitEvent(&event);
+
+		switch (event.type) {
+		case SDL_QUIT:
+			loop = false;
+			break;
+
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				loop = false;
+				break;
+
+			default:
+				break;
+			}
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
